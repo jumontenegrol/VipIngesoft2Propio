96df25223d6252e3e3d3fa2dc33975f0ff1398aa
jumontenegrol <jumontenegrol@unal.edu.co>
2025-11-12 11:54:21 -0500

    Intento 1 de github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/VipIngesoft2Propio/",
   server: {
     port: 5173,
     strictPort: true,
   },
   build: {
     outDir: "../frontend/dist",
+    //outDir: "dist",
   },
   test: {
     globals: true,
